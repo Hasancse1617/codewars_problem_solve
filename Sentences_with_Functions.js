343f2d23b095ddb977656baacd78b0f5c22b8d17
Hasan Ali <hasancseiu16@gmail.com>
2023-04-04 14:24:24 +0600

    added Sentences_with_Functions.js

---
+++ Sentences_with_Functions.js
@@ -0,0 +1,10 @@
+function Adam(n=".") {return "Adam"+n}
+function has(n=".") {return " has "+n}
+function a(n=".") { return "a "+n}
+function dog(n=".") { return "dog"+n}
+function The(n=".") {return "The "+n}
+function name(n=".") {return "name "+n}
+function of(n=".") {return "of "+n}
+function the(n=".") {return "the "+n}
+function is(n=".") {return " is "+n}
+function also(n=".") {return "also "+n}
